a9873981ac4a726da3edd7d8ca24acab2858e987
Lê Đặng Phương Uyên <renyuiko.hamyana@gmail.com>
2023-11-28 03:04:11 +0700

    Add project files.

---
+++ PetCareAndAdoption/Data/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PetCareAndAdoption.Data
+{
+    public class MyDbContext:DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> opt) : base(opt)
+        {
+
+        }
+        #region DbSet
+        public DbSet<UserInfo>? Users { get; set; }
+        #endregion 
+    }
+}
---
+++ PetCareAndAdoption/Data/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PetCareAndAdoption.Data
+{
+    [Table("UserInfo")]
+    public class UserInfo
+    {
+        [Key]
+        [MaxLength(10)]
+        public int userID { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public DateTime birthday { get; set; }
+        public string password { get; set; }
+    }
+}
